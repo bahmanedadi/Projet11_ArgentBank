852656af9bdd7afc555a5c0350e0c1ae96bd849f
Dadi B <benmezianedadi@yahoo.fr>
2024-02-03 22:49:33 +0100

    creation des routes

---
+++ front-end/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import HomeFeatures from "../../components/HomeFeatures/HomeFeature";
+import Banner from "../../components/Banner/Banner";
+function Home() {
+    return (
+        <main>
+         <Banner  />
+        <HomeFeatures />    
+        </main>
+    );
+}
+export default Home;
---
+++ front-end/src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+
+const Login = () => {
+
+  return (
+    <main className="main bg-dark">
+      <LoginForm />
+    </main>
+  );
+};
+
+export default Login;
